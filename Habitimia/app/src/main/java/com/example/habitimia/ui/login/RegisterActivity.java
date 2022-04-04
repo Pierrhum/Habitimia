9801bb87663a5a297b60153fd004639c1d6084b7
Pierrhum <34352456+Pierrhum@users.noreply.github.com>
2022-04-04 18:49:48 +0200

    Register Activity - UI

--- Habitimia/app/src/main/java/com/example/habitimia/ui/login/RegisterActivity.java
+++ Habitimia/app/src/main/java/com/example/habitimia/ui/login/RegisterActivity.java
@@ -3,6 +3,8 @@ package com.example.habitimia.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.habitimia.R;
 
@@ -12,5 +14,17 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final Button loginButton = findViewById(R.id.login_btn);
+        final Button registerButton = findViewById(R.id.register_btn);
+
+        loginButton.setOnClickListener(view -> {
+            finish(); // Go back to login
+        });
+
+        registerButton.setOnClickListener(view -> {
+            Toast.makeText(this, "TODO : Register", Toast.LENGTH_SHORT).show();
+        });
+
     }
 }

733fa40eea82aa7a348ffcb9581804d61f0884cb
Pierrhum <34352456+Pierrhum@users.noreply.github.com>
2022-04-04 13:35:44 +0200

    Splash activity

---
+++ Habitimia/app/src/main/java/com/example/habitimia/ui/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.habitimia.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+import com.example.habitimia.R;
+import com.example.habitimia.ui.login.LoginActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}

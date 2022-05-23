cbb887ef53beba79cd05eeedeeb532eb80e74e47
Albena Stefanova <albena.stefanova@yahoo.com>
2022-05-23 19:43:09 +0200

    dummy quests, still not connected to db

---
+++ habitimia/app/src/main/java/com/example/habitimia/data/adapter/QuestAdapter.java
@@ -0,0 +1,93 @@
+package com.example.habitimia.data.adapter;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.habitimia.R;
+import com.example.habitimia.data.model.Quest;
+import com.example.habitimia.ui.MainActivity;
+
+import java.util.List;
+
+public class QuestAdapter extends RecyclerView.Adapter<QuestAdapter.ViewHolder> {
+
+    private List<Quest> questList;
+    private FragmentActivity activity;
+
+    public QuestAdapter(FragmentActivity activity) {
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_quest_task, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull QuestAdapter.ViewHolder holder, int position) {
+        final Quest item = questList.get(position);
+        holder.task.setText(item.getName());
+        holder.task.setChecked(false);
+        holder.task.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+//                    db.updateStatus(item.getId(), 1);
+                } else {
+//                    db.updateStatus(item.getId(), 0);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return questList.size();
+    }
+
+    public Context getContext() {
+        return activity;
+    }
+
+    public void setTasks(List<Quest> questList) {
+        this.questList = questList;
+//        notifyDataSetChanged();
+    }
+
+    public void deleteItem(int position) {
+        Quest item = questList.get(position);
+//        db.deleteTask(item.getId());
+        questList.remove(position);
+//        notifyItemRemoved(position);
+    }
+
+    public void editItem(int position) {
+        Quest item = questList.get(position);
+        Bundle bundle = new Bundle();
+        bundle.putInt("id", Integer.parseInt(item.getId().toString()));
+        bundle.putString("task", item.getName());
+//        AddNewTask fragment = new AddNewTask();
+//        fragment.setArguments(bundle);
+//        fragment.show(activity.getSupportFragmentManager(), AddNewTask.TAG);
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        CheckBox task;
+
+        ViewHolder(View view) {
+            super(view);
+            task = view.findViewById(R.id.questCheckBox);
+        }
+    }
+}
---
+++ habitimia/app/src/main/java/com/example/habitimia/data/model/Quest.java
@@ -0,0 +1,73 @@
+package com.example.habitimia.data.model;
+
+import java.util.List;
+
+public class Quest {
+
+    private Long id;
+
+    private User user;
+
+    private String name;
+
+    private String details;
+
+    private AdventurerClass difficulty;
+
+    private List<Repetition> repetitions;
+
+    public Quest() {
+    }
+    public Quest(User user, String name, String details, AdventurerClass difficulty) {
+        this.user = user;
+        this.name = name;
+        this.details = details;
+        this.difficulty = difficulty;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public AdventurerClass getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(AdventurerClass difficulty) {
+        this.difficulty = difficulty;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public List<Repetition> getRepetitions() {
+        return repetitions;
+    }
+    public void setRepetitions(List<Repetition> repetitions) {
+        this.repetitions = repetitions;
+    }
+
+
+}

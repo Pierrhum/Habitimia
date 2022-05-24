e1f742642fc156ebd2959327b630f18aa57be693
Pierrhum <34352456+Pierrhum@users.noreply.github.com>
2022-05-24 09:56:33 +0200

    test webhook

--- Habitimia/app/src/main/java/com/example/habitimia/ui/MainActivity.java
+++ Habitimia/app/src/main/java/com/example/habitimia/ui/MainActivity.java
@@ -51,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
 
     User user;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
